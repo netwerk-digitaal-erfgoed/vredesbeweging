b7dfc26d80bedfa6cc0ca908bbb650d9059a4323
Bob Coret <bob.coret+github@gmail.com>
2022-12-06 18:45:30 +0100

    consistent use of schema with http (not https)

--- src/csv2nt.py
+++ src/csv2nt.py
@@ -11,7 +11,7 @@ with open('data/Vredesbeweging.tsv', mode='r') as csv_file:
 
         if row["title"]:
             title=row["title"].replace('"', r'\"');
-            print(f'{uri} <https://schema.org/name> "{title}" .')
+            print(f'{uri} <http://schema.org/name> "{title}" .')
 
         for i in range(1,15):
             if row[f"creato {i}"]:
@@ -21,12 +21,12 @@ with open('data/Vredesbeweging.tsv', mode='r') as csv_file:
                     
                     if row[f"creato uri {i}"]:
                         curi=row[f"creato uri {i}"]
-                        print(f'{uri} <https://schema.org/creator> <{curi}> .')
+                        print(f'{uri} <http://schema.org/creator> <{curi}> .')
                         print(f'<{curi}> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Person> .')
                         print(f'<{curi}> <http://www.w3.org/2000/01/rdf-schema#label> "{creato}" .')
                         print(f'<{curi}> <http://schema.org/name> "{creato}" .')
                     else:
-                        print(f'{uri} <https://schema.org/creator> _:creator_{creatorid} .')
+                        print(f'{uri} <http://schema.org/creator> _:creator_{creatorid} .')
                         print(f'_:creator_{creatorid} <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Person> .')
                         print(f'_:creator_{creatorid} <http://www.w3.org/2000/01/rdf-schema#label> "{creato}" .')
                         print(f'_:creator_{creatorid} <http://schema.org/name> "{creato}" .')
@@ -34,30 +34,30 @@ with open('data/Vredesbeweging.tsv', mode='r') as csv_file:
             
         if row["subjec"]: 
             subjec=row["subjec"].replace('"', r'\"');
-            print(f'{uri} <https://schema.org/about> "{subjec}" .')
+            print(f'{uri} <http://schema.org/about> "{subjec}" .')
             
         if row["descri"]: 
             descri=row["descri"].replace('"', r'\"').replace('\n', r'\\n');
-            print(f'{uri} <https://schema.org/abstract> "{descri}" .')
+            print(f'{uri} <http://schema.org/abstract> "{descri}" .')
 
         if row["losse"]: 
             losse=row["losse"].replace('"', r'\"').replace('\n', r'\\n');
-            print(f'{uri} <https://schema.org/abstract> "{losse}" .')
+            print(f'{uri} <http://schema.org/abstract> "{losse}" .')
 
         if row["langua"]: 
             languas=row["langua"].split(";")
             for langua in languas:
                 lang=langua.strip() 
-                print(f'{uri} <https://schema.org/inLanguage> _:lang_{lang} .')
+                print(f'{uri} <http://schema.org/inLanguage> _:lang_{lang} .')
 
         if row["publis"]: 
             publis=row["publis"].replace('"', r'\"');
-            print(f'{uri} <https://schema.org/publisher> "{publis}" .')
+            print(f'{uri} <http://schema.org/publisher> "{publis}" .')
             # TODO zou eigenlijk Organization moeten zijn
             
         if row["source"]: 
             source=row["source"].replace('"', r'\"');
-            print(f'{uri} <https://schema.org/sourceOrganization> "{source}" .')
+            print(f'{uri} <http://schema.org/sourceOrganization> "{source}" .')
             # TODO zou eigenlijk Organization moeten zijn
             
         if row["find"]: 
@@ -70,24 +70,24 @@ with open('data/Vredesbeweging.tsv', mode='r') as csv_file:
 
         if row["date"]:
             date=row["date"].replace('"', r'\"');
-            print(f'{uri} <https://schema.org/dateCreated> "{date}" .')
+            print(f'{uri} <http://schema.org/dateCreated> "{date}" .')
 
         if row["contri"]: 
             contri=row["contri"].replace('"', r'\"').replace('\n', r'');
-            print(f'{uri} <https://schema.org/contributor> "{contri}" .')
+            print(f'{uri} <http://schema.org/contributor> "{contri}" .')
             # TODO zou eigenlijk Organization moeten zijn
             
         if row["identi"]: 
             identi=row["identi"].replace('"', r'\"');
-            print(f'{uri} <https://schema.org/identifier> "{identi}" .')
+            print(f'{uri} <http://schema.org/identifier> "{identi}" .')
   
         if row["dmcreated"]: 
             dmcreated=row["dmcreated"].replace('"', r'\"');
             print(f'{uri} <https://cdm21069.contentdm.oclc.org/digital/collection/p21069coll13/id/def/dmcreated> "{dmcreated}" .')
  
-        print(f'{uri} <https://schema.org/image> <https://cdm21069.contentdm.oclc.org/digital/api/singleitem/collection/p21069coll13/id/{dmrecord}/thumbnail> .')
-        print(f'{uri} <https://schema.org/mainEntityOfPage> <https://cdm21069.contentdm.oclc.org/digital/collection/p21069coll13/id/{dmrecord}> .')
-        print(f'{uri} <https://schema.org/url> <https://cdm21069.contentdm.oclc.org/digital/api/collection/p21069coll13/id/{dmrecord}/download> .')
+        print(f'{uri} <http://schema.org/image> <https://cdm21069.contentdm.oclc.org/digital/api/singleitem/collection/p21069coll13/id/{dmrecord}/thumbnail> .')
+        print(f'{uri} <http://schema.org/mainEntityOfPage> <https://cdm21069.contentdm.oclc.org/digital/collection/p21069coll13/id/{dmrecord}> .')
+        print(f'{uri} <http://schema.org/url> <https://cdm21069.contentdm.oclc.org/digital/api/collection/p21069coll13/id/{dmrecord}/download> .')
 
 
 # NB: in brondata komen enkele fouten voor:
